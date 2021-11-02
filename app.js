68e8cd5c7d01bd61146bf54bebdbec70805bad15
AtsuyaSakamoto <sakamo.070501@gmail.com>
2021-11-02 15:59:36 +0900

    DB接続のモジュール化

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ app.set("view engine", "ejs");
 // サーバー情報の隠蔽
 app.disable("x-powered-by");
 
+// Expose global method to view engine
+app.use((req, res, next) => {
+  res.locals.moment = require("moment");
+  res.locals.padding = require("./lib/math/math.js").padding;
+  next();
+});
+
 // Static resource rooting.
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
@@ -21,6 +28,44 @@ app.use(accesslogger());
 
 // Dynamic resource rooting.
 app.use("/", require("./routes/index.js"));
+app.use("/test", async (req, res, next) => {
+  const { MYSQLClient, sql } = require("./lib/database/client.js");
+  // client.jsへの切り分け
+
+  // const { promisify } = require("util");
+  // const path = require("path");
+  // const { sql } = require("@garafu/mysql-fileloader")({root:path.join(__dirname,"./lib/database/sql")});
+  // const config = require("./config/mysql.config.js");
+  // const mysql = require("mysql");
+  // const con = mysql.createConnection({
+  //   host: config.HOST,
+  //   port: config.PORT,
+  //   user: config.USERNAME,
+  //   password:config.PASSWORD,
+  //   database: config.DATABASE
+  // });
+  // const client = {
+  //   connect: promisify(con.connect).bind(con),
+  //   query: promisify(con.query).bind(con),
+  //   end: promisify(con.end).bind(con)
+  // };
+  let data;
+
+  try {
+    // poolで不要に
+    // await MYSQLClient.connect();
+    data = await MYSQLClient.executeQuery(await sql("SELECT_SHOP_BASIC_BY_ID"),[1]);
+    console.log(data);
+  } catch (err) {
+    next(err);
+  }
+  // 下記もpoolで不用に
+  // finally {
+  //   await MYSQLClient.end();
+  // }
+
+  res.end("ok");
+});
 
 // Set application log.
 app.use(applicationlogger());
